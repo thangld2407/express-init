3fe0dd3e6ffedca1fb9494570dbe359ad90a8ea1
Winx - Broken <thangld2407@gmail.com>
2022-10-04 17:29:38 +0700

    API Bitmex

--- controller/DataController.js
+++ controller/DataController.js
@@ -1,6 +1,7 @@
 const axiosInstance = require('../api/axiosInstance');
 const DataModel = require('../model/DataModel');
 const moment = require('moment');
+const { default: axios } = require('axios');
 module.exports = {
 	async postData(req, res) {
 		const { symbol, windowSize } = req.body;
@@ -51,21 +52,97 @@ module.exports = {
 		}
 	},
 	async getDataStatistic(req, res) {
-		const { symbol, windowSize } = req.body;
+		const { symbol, interval, startTime, endTime, limit } = req.body;
 		const params = {
 			symbol: symbol.toUpperCase(),
-			windowSize
+			interval,
+			startTime,
+			endTime,
+			limit
 		};
+		const result = [];
 		try {
-			const response = await axiosInstance.get('/api/v3/ticker', {
+			const { data } = await axiosInstance.get('/api/v3/klines', {
 				params
 			});
+			if (data && data.length > 0) {
+				data.map(item => {
+					result.push({
+						open_time: moment(item[0]).format('YYYY-MM-DD HH:mm:ss'),
+						open_price: item[1],
+						high_price: item[2],
+						low_price: item[3],
+						close_price: item[4],
+						volume: item[5],
+						close_time: moment(item[6]).format('YYYY-MM-DD HH:mm:ss')
+					});
+				});
+			}
 			res.json({
 				message: 'Data fetched successfully',
-				data: response.data
+				data: result
 			});
 		} catch (error) {
 			res.json({ error: error.message });
+		}
+	},
+
+	async getDataBitmex(req, res) {
+		try {
+			const query = req.query;
+			let result = [];
+			const { data } = await axios.get('https://www.bitmex.com/api/udf/history', {
+				params: query
+			});
+			if (data.s === 'no_data') {
+				return res.status(200).json({
+					error_code: 100,
+					error_message: 'No data'
+				});
+			}
+
+			result.push({
+				time: {
+					prev: moment(data.t[0] * 1000)
+						.utc(false)
+						.format('YYYY-MM-DD HH:mm:ss'),
+					curent: moment(data.t[1] * 1000)
+						.utc(false)
+						.format('YYYY-MM-DD HH:mm:ss')
+				},
+				close_price: {
+					prev: data.c[0],
+					curent: data.c[1]
+				},
+				open_price: {
+					prev: data.o[0],
+					curent: data.o[1]
+				},
+				high_price: {
+					prev: data.h[0],
+					curent: data.h[1]
+				},
+				low_price: {
+					prev: data.l[0],
+					curent: data.l[1]
+				},
+				volume: {
+					prev: data.v[0],
+					curent: data.v[1]
+				}
+			});
+
+			res.json({
+				message: 'Data fetched successfully',
+				symbol: query.symbol,
+				data: result
+			});
+		} catch (error) {
+			res.status(500).json({
+				error: error.message,
+				error_code: 500,
+				error_bitmex: error
+			});
 		}
 	}
 };
--- routes/api/module/binance/index.js
+++ routes/api/module/binance/index.js
@@ -1,9 +1,15 @@
-const { postData, getData, getDataStatistic } = require('../../../../controller/DataController');
+const {
+	postData,
+	getData,
+	getDataStatistic,
+	getDataBitmex
+} = require('../../../../controller/DataController');
 
 const routerBinance = require('express').Router();
 
 routerBinance.post('/binance/statistic', postData);
 routerBinance.post('/binance', getData);
 routerBinance.post('/binance/ticker', getDataStatistic);
+routerBinance.post('/bitmex/list', getDataBitmex);
 
 module.exports = routerBinance;
